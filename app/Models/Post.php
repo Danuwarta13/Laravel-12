ecc34f428e3a20071cf6f4e5b0190a0ac191ed3f
Danuwarta13 <danujr650@gmail.com>
2025-12-15 14:58:25 +0700

    Model Factories

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    use HasFactory;
+
     protected $guarded = [];
-
-
 
     // protected $table = 'blog_posts';
 
