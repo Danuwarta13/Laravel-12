d79fac380f1158d3603dde235cde84988e395e5b
Danuwarta13 <danujr650@gmail.com>
2025-12-19 15:28:38 +0700

    Query Scopes

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -20,8 +20,8 @@
                          <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
                              <img class="mr-4 w-16 h-16 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="{{ $post->author->name }}">
                              <div>
-                                 <a href="/authors/{{ $post->author->username }}" rel="author" class="block text-xl font-bold text-gray-900 mb-1 hover:underline dark:text-white">{{ $post->author->name }}</a>
-                                 <a href="/categories/{{ $post->category->slug }}">
+                                 <a href="/posts?author={{ $post->author->username }}" rel="author" class="block text-xl font-bold text-gray-900 mb-1 hover:underline dark:text-white">{{ $post->author->name }}</a>
+                                 <a href="/posts?category={{ $post->category->slug }}">
                                      <span class=" {{ $post->category->color }} hover:scale-105 transition-all text-gray-600 mb-1 text-xs font-medium inline-flex items-center px-2.5 py-1 rounded-sm dark:bg-primary-200 dark:text-primary-800">
                                          {{$post->category->name}}
                                      </span>
--- routes/web.php
+++ routes/web.php
@@ -13,31 +13,14 @@ Route::get('/posts', function () {
 
     // $posts = Post::with(['category', 'author'])->latest()->get();
 
-    $posts = Post::latest();
-
-    if (request('search')) {
-        $posts->where('title', 'like', '%' . request('search') . '%');
-    }
+    $posts = Post::latest()->filter(request(['search', 'category', 'author']))->get();
 
     // dd($posts);
-    return view('posts', ['title' => 'Blog page', 'posts' => $posts->get()]);
+    return view('posts', ['title' => 'Blog page', 'posts' => $posts]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', ['title' => 'Single Post', 'post' => $post]);
-});
-
-Route::get('/authors/{user:username}', function (User $user) {
-    // dd($posts);
-    // $posts = $user->posts->load(['category', 'author']);
-
-    return view('posts', ['title' => count($user->posts) . ' Article By ' . $user->name, 'posts' => $user->posts]);
-});
-
-Route::get('/categories/{category:slug}', function (Category $category) {
-    // $posts = $category->posts->load(['category', 'author']);
-
-    return view('posts', ['title' => count($category->posts) . ' Category: ' . $category->name, 'posts' => $category->posts]);
 });
 
 Route::get('/about', function () {
